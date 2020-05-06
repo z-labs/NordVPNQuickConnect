0305cc17f1384f8324a43487e4a64b3c26c41cb4
z-labs <36811921+z-labs@users.noreply.github.com>
2020-05-06 18:51:19 +0200

    Add files via upload

---
+++ nqc_main.py
@@ -0,0 +1,133 @@
+import sys
+import os
+
+import simplelogging
+
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import QFileInfo, QSettings
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QDesktopWidget)
+
+from UI_maindlg import Ui_Dialog
+
+revision = 1.0
+
+LOG_CONSOLE_FORMAT = "%(log_color)s%(asctime)s [%(levelname)-8s] %(filename)20s(%(lineno)3s):: %(message)s%(reset)s"
+LOG_FILE_FORMAT = "%(asctime)s [%(levelname)-8s] %(filename)20s(%(lineno)3s):: %(message)s"
+log = simplelogging.get_logger(file_format=LOG_FILE_FORMAT, console_format=LOG_CONSOLE_FORMAT, file_name='log.log')
+
+class AppWindow(QMainWindow, Ui_Dialog):
+    def center(self):
+        framegm = self.frameGeometry()
+        centrepoint = QDesktopWidget().availableGeometry().center()
+        framegm.moveCenter(centrepoint)
+        self.move(framegm.topLeft())
+
+    def __init__(self):
+        super(AppWindow, self).__init__()
+        self.setupUi(self)
+        self.show()
+        self.center()
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.setFont(font)
+
+        self.swSelection.setCurrentIndex(0)
+        self.cbxCode.setVisible(False)
+        self.resize(560, 320)
+
+        self.LoadCountries()
+
+        self.lwOptions.clicked.connect(self.OnListClick)
+        self.pbClose.clicked.connect(self.OnCloseClick)
+        self.cbViewTerminal.stateChanged.connect(self.OnViewTerminalChange)
+
+        # Quick functions
+        self.pbConnectQuick.clicked.connect(self.OnConnectQuickClick)
+        self.pbDisconnectQuick.clicked.connect(self.OnDisconnectQuickClick)
+
+        # Select from lists
+        self.pbConnectList.clicked.connect(self.OnConnectListClick)
+        self.pbDisconnectList.clicked.connect(self.OnDisconnectListClick)
+        self.cbxCountry.currentTextChanged.connect(self.LoadCities)
+
+        # Select by server code
+        self.pbConnectCode.clicked.connect(self.OnConnectCodeClick)
+        self.pbDisconnectCode.clicked.connect(self.OnDisconnectCodeClick)
+
+    def LoadCountries(self):
+        self.cbxCountry.clear()
+        self.cbxCountry.addItem('Select Country')
+        c = self.NordCommand('nordvpn countries')
+        t = c.split('\n')
+        countries = t[6].split(', ')
+        self.cbxCountry.addItems(countries)
+
+    def LoadCities(self):
+        self.pbConnectList.setEnabled(True)
+        self.cbxCity.clear()
+        self.cbxCity.addItem('Select City')
+        c = self.NordCommand('nordvpn cities {}'.format(self.cbxCountry.currentText()))
+        t = c.split('\n')
+        cities = t[6].split(', ')
+        self.cbxCity.addItems(cities)
+
+    def OnViewTerminalChange(self):
+        if self.cbViewTerminal.isChecked():
+            self.resize(560, 560)
+        else:
+            self.resize(560, 320)
+
+    def OnCloseClick(self):
+        QtCore.QCoreApplication.instance().quit()
+
+    def OnListClick(self):
+        i = self.lwOptions.currentIndex().row()
+        self.swSelection.setCurrentIndex(i)
+
+    def OnConnectQuickClick(self):
+        self.NordConnectQuick()
+
+    def OnDisconnectQuickClick(self):
+        self.NordDisconnect()
+
+    def OnConnectListClick(self):
+        c1 = ''
+        if self.cbxCountry.currentIndex() > 0:
+            c1 = self.cbxCountry.currentText()
+        if self.cbxCity.currentIndex() == 0:
+            c2 = ''
+        else:
+            c2 = self.cbxCity.currentText()
+
+        self.NordConnectByCountry(c1, c2)
+
+    def OnDisconnectListClick(self):
+        self.NordDisconnect()
+
+    def OnConnectCodeClick(self):
+        self.NordCommand('nordvpn connect {}'.format(self.leCode.text()))
+
+    def OnDisconnectCodeClick(self):
+        self.NordDisconnect()
+
+    def NordCommand(self, cmd):
+        reply = os.popen(cmd).read()
+        log.info(reply)
+        self.pteLog.appendPlainText(reply)
+        return reply
+
+    def NordConnectQuick(self):
+        self.NordCommand('nordvpn connect')
+
+    def NordDisconnect(self):
+        self.NordCommand('nordvpn disconnect')
+
+    def NordConnectByCountry(self, Country, City):
+        self.NordCommand('nordvpn connect {} {}'.format(Country, City))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = AppWindow()
+    w.show()
+    sys.exit(app.exec_())
---
+++ readme.md
@@ -0,0 +1,62 @@
+# [NordVPN](https://nordvpn.com)QuickConnect
+
+![Screenshot_Quick](screenshots/Screenshot_Quick.png)
+
+![Screenshot_List](screenshots/Screenshot_List.png)
+
+![Screenshot_Server](screenshots/Screenshot_Server.png)
+
+## Introduction
+[NordVPN](https://nordvpn.com) QuickConnect is a desktop utility that gives easy user interface to replace the CLI that comes with [NordVPN](https://nordvpn.com) on linux machines
+
+The program was created using PyQT5 and python3. Requirement listed below:
+colorlog==4.0.2
+pkg-resources==0.0.0
+PyQt5==5.14.2
+PyQt5-sip==12.7.2
+PyQt5-stubs==5.14.2.2
+simplelogging==0.10.0
+
+To install requirements execute in terminal 
+```bash
+pip install requirements.txt
+```
+
+## Development/Testing environment
+* The program was tested on a Dell Latitude 6420 laptop running [Ubuntu](https://ubuntubudgie.org/) 18.04
+* [Budgie Desktop](https://ubuntubudgie.org/)
+* [NordVPN](https://nordvpn.com)version is 3.7.1
+
+![Screenshot_OS](screenshots/OS About.png)
+
+![Screenshot_OS](screenshots/terminal.png)
+
+
+## Disclaimer
+This application has no official connection to [NordVPN](https://nordvpn.com). 
+
+The developer has no connection to [NordVPN](https://nordvpn.com)
+ 
+> **_!! NOTE:_**  USE OF THIS APPLICATION IS AT THE USERS OWN RISK
+
+## Installation
+
+This readme assumes that the [NordVPN](https://nordvpn.com) app has been installed as indicated on the [NordVPN](https://nordvpn.com/download/linux/) website
+
+1. Download a copy from [Github](path to project)
+2. Extract to personal folder
+3. Open a terminal and type Inline `nordvpn login`
+4. Enter registration details
+5. Wait for login is successful message
+6. Copy the `NordVPNQuickConnect.desktop` to your desktop
+7. Open the `NordVPNQuickConnect.desktop` file in a text editor. Enter the complete path the `nqc_main.py` where indicated in the file
+8. Right click on the file and select Properties. Under permissions, check the `Allow Executing File As Program` checkbox
+9. Now when you click on the Desktop file it should open the program   
+10. To log out of [NordVPN](https://nordvpn.com), open a Terminal and type `nordvpn logout`
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[GPL-3.0](https://choosealicense.com/licenses/gpl-3.0/)
+
